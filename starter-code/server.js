4b781e0ab31d4316ad75d0288b2dc1386dfcc231
radenska <yana_radenska@yahoo.com>
2017-01-10 15:24:29 -0800

    set up server, dependencies, todos on server js file, added 404 page

--- starter-code/server.js
+++ starter-code/server.js
@@ -1,20 +1,36 @@
 'use strict';
 
-// TODO: Initialize your project using NPM to create and populate a package.json file
+// DONE: Initialize your project using NPM to create and populate a package.json file
+const express = require('express');
+const app = express();
 
-// TODO: Require the Express package that you installed via NPM, and instantiate the app
+// DONE: Require the Express package that you installed via NPM, and instantiate the app
 // Remember to install express, and be sure that it's been added to your package.json as a dependency
 
 const PORT = process.env.PORT || 3000;
 
-// TODO: Include all of the static resources as an argument to app.use()
+// DONE: Include all of the static resources as an argument to app.use()
 
-app.get('*', function(request, response) {
-  // TODO: Using the response object, send the index.html file back to the user
+app.use(express.static('./public'));
+
+app.get('index.html', function(request, response) {
+    // DONE: Using the response object, send the index.html file back to the user
+  response.sendFile('index.html', {root: './public'});
 });
 
-// TODO: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
+// DONE: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
+
+app.get('new.html', function(request, response) {
+  response.sendFile('new.html', {root: './public'});
+});
+
+app.get('*', function(request, response) {
+  // console.log('New request: ', request.url);
+  response.status(404).sendFile('404.html', {root: './public'});
+});
+
 
 app.listen(PORT, function() {
-  // TODO: Log to the console a message that lets you know which port your server has started on
+  // DONE: Log to the console a message that lets you know which port your server has started on
+  console.log('Server is up and running on port ' + PORT + ' and can be accessed at localhost:' + PORT);
 });
